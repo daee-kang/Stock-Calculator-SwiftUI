97adebce837827c83d87af7cf2e198087a755658
daee <thundrnuts@gmail.com>
2020-06-08 02:28:14 -0700

    Initial Commit

---
+++ Stock Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stock Calculator
+//
+//  Created by Daee Kang on 6/8/20.
+//  Copyright © 2020 Daee Kang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
